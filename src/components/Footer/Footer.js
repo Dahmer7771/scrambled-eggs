e20c356524d07645d7b66a867811b2e7c403182a
valeravarchak <valeraavarchak@gmail.com>
2019-10-29 22:30:34 +0300

    Added styles in RcipesSectionItem.css, Header.css Created Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import Logo from '../../img/logo.png';
 
 class Footer extends React.Component{
   constructor(props) {
@@ -12,9 +13,35 @@ class Footer extends React.Component{
   render() {
     return (
       <footer className="container-fluid">
-        <div className="row">
-          <div className="col-xl-4">
-
+        <figure>
+          <img className="logo-footer"  src={Logo} alt=""/>
+        </figure>
+        <p className="author">
+          @ Сreated by 6 subgroups specifically for the TSS lesson
+        </p>
+        <div className="footer-content row justify-content-center">
+          <div className="contacts col-xl-5">
+              <h4>
+                Contact
+              </h4>
+              <div className="phone">
+                <p><i className="zmdi zmdi-smartphone-ring"></i>+380632882882</p>
+                <p><i className="zmdi zmdi-smartphone-ring"></i>+380632882882</p>
+                <a href="https://mail.google.com"><i className="zmdi zmdi-google-plus-box"></i>valerka@gmail.com</a>
+              </div>
+          </div>
+          <div className="social-network col-xl-5">
+            <h4>
+              Social network
+            </h4>
+            <div className="social-network_content">
+              <a href="#"><i className="zmdi zmdi-instagram"></i></a>
+              <a href="#"><i className="zmdi zmdi-twitter"></i></a>
+              <a href="#"><i className="zmdi zmdi-vk"></i></a>
+              <a href="#"><i className="zmdi zmdi-facebook"></i></a>
+              <a href="#"><i className="zmdi zmdi-skype"></i></a>
+              <a href="#"><i className="zmdi zmdi-stackoverflow"></i></a>
+            </div>
           </div>
         </div>
       </footer>
